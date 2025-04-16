72b5729cfe713531407fffc155a25a81c4a91401
Kyuri-dao <daiki0314.dev@gmail.com>
2025-04-16 23:55:56 +0900

    Added some text.

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -7,6 +7,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import github_logo from "./assets/github-mark-white.svg";
 import qiita_logo from "./assets/qiita-white-icon.png"; 
 import ts_logo from "./assets/ts_logo.svg";
+import react_logo from "./assets/react.svg";
 import cs_logo from "./assets/c_sharp_logo.png";
 import my_rn_pj from "./assets/my_rn_project.png";
 
@@ -22,6 +23,7 @@ function App() {
         <header className='main-header'>
           <div className='title'>
             <p className='title-index'>Kyuri-dao/Dev</p>
+            <p>情報系学徒</p>
           </div>
           
           <div className='title-menu'>
@@ -36,17 +38,25 @@ function App() {
 
         <div className='child-container'>
           <div className='swipe-area'>
-            <p>My Projects</p>
+            <p>My develop status</p>
             <Swiper>
               <SwiperSlide>
                 <div  className='swipe-index'>
                   <div className='swipe-title'>
-                    <p>TypeScript/JavaScript</p>
-                    <img src={ts_logo} alt="ts logo" className='ts-logo'/>
+                  <p>TypeScript/JavaScript</p>
+                    <div className='swipe-image-area'>
+                      <img src={ts_logo} alt="ts logo" className='ts-logo'/>
+                      <p>&</p>
+                      <img src={react_logo} alt="React logo" className='react-logo'/>
+                    </div>
                   </div>
                   <div className='swipe-explain'>
                     <p>TS & React Nativeを用いたアプリ制作</p>
+                    <p className='swipe-explain-mini'>・簡易的なユーザー情報</p>
+                    <p className='swipe-explain-mini'>・シラバス</p>
+                    <p className='swipe-explain-mini'>・列車運行情報</p>
                     <img src={my_rn_pj} alt="React Nativeで作られたアプリの画像" className='my_rn_pj'/>
+                    <p className='swipe-explain-mini'>iOS18での動作テストの様子</p>
                   </div>
                 </div>
               </SwiperSlide>
@@ -65,6 +75,10 @@ function App() {
             </Swiper>
 
           </div>
+        </div>
+
+        <div className='link-area'>
+          <p>My link list</p>
         </div>
       </div>
     </>
